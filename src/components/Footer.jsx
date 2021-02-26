694e890afa820e0ded9a1fac77d0e80bb66ae446
aderonn <apbeuroonly@gmail.com>
2021-02-26 16:26:02 +0300

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return <footer className="page-footer deep-purple lighten-2">
+    <div className="footer-copyright">
+      <div className="container">
+      © {new Date().getFullYear()} made by Aderon
+      <a className="grey-text text-lighten-4 right" href="https://github.com/aderonn/fortnite-react-shop" target='_blank' rel="noreferrer">More Links</a>
+      </div>
+    </div>
+  </footer>
+}
+
+export {Footer}
